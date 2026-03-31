6be14d96390d2b6c10f6bc8e04ec593d40b4b750
Blankaert <blankaertlouis@outlook.com>
2026-04-01 01:34:04 +0200

    C03 strstr fini et C04 commencé en parallel

--- C04/ft_atoi.c
+++ C04/ft_atoi.c
@@ -1,33 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   strstr.c                                           :+:      :+:    :+:   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: louisblankaert <louisblankaert@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/03/31 23:29:30 by louisblanka       #+#    #+#             */
-/*   Updated: 2026/04/01 00:45:52 by louisblanka      ###   ########.fr       */
+/*   Created: 2026/04/01 01:18:52 by louisblanka       #+#    #+#             */
+/*   Updated: 2026/04/01 01:27:45 by louisblanka      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char *ft_strstr(char *str, char *to_find)
+int ft_atoi(char *str)
 {
 	int i;
-	int j;
+	int res;
+	int sign;
 
-	if (to_find[0] == '\0')
-		return str;
 	i = 0;
-	while (str[i])
-	{
-		j = 0;
-		while (str[i + j] && str[i + j] == to_find[j])
-		{
-			j++;
-		}
-		if (to_find[j] == '\0')
-			return &str[i];
+	res = 0;
+	sign = 1;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
 		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++; 
 	}
-	return 0;
+	while (str[i] >= '0' && str[i] <= '9')
+		{
+			res = res * 10 + (str[i] - '0');
+			i++;
+		}
+	return (res * sign); 
 }
+// #include <stdio.h>
+// int main(void)
+// {
+// 	printf("%d\n", ft_atoi("42"));
+// 	return 0;
+// }
